b038b66fe8cec079e526486d3826b499342d25ee
Krzysztof Sadowski <k.sadowskik@gmail.com>
2014-01-01 20:50:20 +0100

    add nuspec

--- Hadoop.Client/Properties/AssemblyInfo.cs
+++ Hadoop.Client/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Hadoop.Client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".NET Hadoop Client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("KS")]
 [assembly: AssemblyProduct("Hadoop.Client")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
